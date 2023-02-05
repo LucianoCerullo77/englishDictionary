980a83cf59e65191b9a8c7aaf2c0b1c4c3193a08
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-02-05 00:52:14 -0300

    audio added, also notfound results were added to the logic, comments maded for better explanation

--- index.js
+++ index.js
@@ -7,22 +7,39 @@ const audio = document.getElementById("audio");
 
 const fetchAPI = async (word) => {
   try {
+    // Containers Display appears
     infoText.style.display = "block";
     meaningContainer.style.display = "none";
     infoText.innerText = `Searching the meaning of "${word}"`;
 
+    // Fetching Dictionary API
     const URL = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     const result = await fetch(URL).then((res) => res.json());
 
-    infoText.style.display = "none";
-    meaningContainer.style.display = "block";
-    wordTitle.innerText = result[0].word;
-    meaningTitle.innerText = result[0].meanings[0].definitions[0].definition;
+    // Showing Results
+    if (result.title) {
+      // No Results found
+      meaningContainer.style.display = "block";
+      wordTitle.innerText = word;
+      meaningTitle.innerText = "Not Found";
+      audio.style.display = "none";
+    } else {
+      // Containers Display disappears
+      infoText.style.display = "none";
+      meaningContainer.style.display = "block";
+
+      // Results found
+      wordTitle.innerText = result[0].word;
+      meaningTitle.innerText = result[0].meanings[0].definitions[0].definition;
+      audio.style.display = "inline-block";
+      audio.src = result[0].phonetics[0].audio;
+    }
   } catch (err) {
     console.log(err);
   }
 };
 
+// Getting the info in the Searchbar
 inputSearch.addEventListener("keyup", (e) => {
   if (e.target.value && e.key === "Enter") {
     fetchAPI(e.target.value);
